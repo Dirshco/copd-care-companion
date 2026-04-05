eed89e2a65e381c8fc1129837019c848808004a9
Dirshco <152452879+Dirshco@users.noreply.github.com>
2026-04-04 23:37:17 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+{
+  "name": "COPD Care Companion",
+  "short_name": "COPD Care",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#f4f6f8",
+  "theme_color": "#0056b3",
+  "icons": [{"src":"./icons/icon-192x192.png","sizes":"192x192","type":"image/png"},{"src":"./icons/icon-512x512.png","sizes":"512x512","type":"image/png"}]
+}
